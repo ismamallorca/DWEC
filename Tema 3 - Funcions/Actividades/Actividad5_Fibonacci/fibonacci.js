36f73ecdbbd5dd87a5e22fbf57f331793ee01410
= <=>
2021-11-08 19:58:54 +0100

    update

---
+++ Tema 3 - Funcions/Actividades/Actividad5_Fibonacci/fibonacci.js
@@ -0,0 +1,20 @@
+
+function fibonacci(num) {
+    if (num < 2) {
+        return num;
+    }
+    else {
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+}
+
+const nTerms = prompt('Introduce un número.');
+
+if (nTerms <= 0) {
+    console.log('Introduce un número positivo.');
+}
+else {
+    for (let i = 0; i < nTerms; i++) {
+        console.log(fibonacci(i));
+    }
+}
