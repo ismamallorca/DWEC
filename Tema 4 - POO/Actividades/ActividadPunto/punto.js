9206b29bcd2020a2795e8f5a68d0db7737ee0907
= <=>
2021-11-18 19:41:30 +0100

    update

--- Tema 4 - POO/Actividades/ActividadPunto/punto.js
+++ Tema 4 - POO/Actividades/ActividadPunto/punto.js
@@ -1,5 +1,34 @@
 class Punto {
     constructor(CoordX, CoordY) {
-        
+        this.X = CoordX;
+        this.Y = CoordY;
+    }
+
+    cambiar(NX, NY) {
+        this.X = NX;
+        this.Y = NY;
+    }
+
+    toString() {
+        return "(" + this.X + "," + this.Y + ")";
+    }
+
+    copia() {
+        return new Punto(this.X, this.Y);
+    }
+
+    suma(punto) {
+        return new Punto(this.X + punto.X, this.Y + punto.Y);
+    }
+
+    calcularDistancia(punto) {
+        let horizontal = (punto.X) - (this.X);
+        let vertical = (punto.Y) - (this.Y);
+        let elevadoX = Math.pow(horizontal, 2);
+        let elevadoY = Math.pow(vertical, 2);
+        let raiz = Math.sqrt(elevadoX + elevadoY);
+        return raiz.toFixed(2);
     }
 }
+
+export {Punto};
