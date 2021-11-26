f68707131622927968cb397c4c5f5905d4404295
= <=>
2021-11-26 17:38:07 +0100

    update

--- Tema 4 - POO/Actividades/ActividadComunidadPropietarios/Actividad3/actividad3.js
+++ Tema 4 - POO/Actividades/ActividadComunidadPropietarios/Actividad3/actividad3.js
@@ -86,5 +86,79 @@ function Edificio(tipoVia, nombreVia, numeroEdificio, codigoPostal) {
 function solicitar() {
     let correcto = false;
     let tipoVia = prompt("Introduce el tipo de vía que desees: ");
-    let nombreVia
+    let nombreVia = prompt("Introduce el nombre de la vía: ");
+    let numeroEdificio = prompt("Introduce el número del edificio que quieras");
+
+    // Obligamos al usuario a introducir el CP SÓLO con números
+    do {
+        let codigoPostal = prompt("Introduce el código postal: ");
+        if (Number(codigoPostal)) {
+            correcto = true;
+            var nuevoEdificio = new Edificio(tipoVia, nombreVia, numeroEdificio, codigoPostal)
+        } else {
+            alert("Formato incorrecto. Por favor, escribe el CP con únicamente números");
+        }
+    } while (!correcto)
+
+    // Pedimos al usuario que introduzca planta, puerta y propietario
+    // Creamos la variable stop para cancelar si el usuario así lo desea
+    let stop = false;
+    do {
+        let nueva_planta = prompt("Introduce el número de una planta: ")
+        if (nueva_planta === null) {
+            stop = true
+        } else {
+            let nueva_puerta = prompt("Introduce el número de la puerta: ")
+            if (nueva_puerta === null) {
+                stop = true
+
+                // Con el siguiente bucle comprobamos que la puerta que ha introducido
+                // el usuario sea un número y que no sea negativo
+            } else if (Number(nueva_puerta) && nueva_puerta >= 0) {
+                let nuevo_propietario = prompt("Introduce el nombre de un propietario: ")
+                if (nuevo_propietario === null) {
+                    stop = true
+
+                    // Añadimos todos los datos recogidos y avisamos al usuario
+                    // que los datos han sido introducidos correctamente
+                } else {
+                    nuevoEdificio.añadirPlanta(nueva_planta);
+                    nuevoEdificio.añadirPuerta(nueva_puerta);
+                    nuevoEdificio.añadirPropietario(nuevo_propietario);
+                    alert("Los datos han sido introducidos correctamente :)");
+                }
+            } else {
+                alert("La puerta que has introducida no es correcta. Inténtalo de nuevo")
+            }
+        }
+    } while (!stop)
+    return nuevoEdificio;
 }
+
+// Insertamos los datos al div para mostrarlos por pantalla
+let div = document.getElementById("resul");
+let edificio3 = solicitar();
+
+// Insertamos los saltos de línea y tabulaciones necesarios
+let espacios = nuevo_Edificio.imprimirTodosPropietarios().split("\n").join("<br>");
+let tabulaciones = espacios.split("\t").join("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;")
+
+// Mostramos por pantalla los datos
+div.innerHTML += "<p>" + edificio3.imprimirTipoVia() + "</p>"
+div.innerHTML += "<p>" + edificio3.imprimirNombreVia() + "</p>"
+div.innerHTML += "<p>" + edificio3.imprimirNumeroEdificio() + "</p>"
+div.innerHTML += "<p>" + edificio3.imprimirCodigoPostal() + "</p>"
+div.innerHTML += "<p>" + tabulaciones + "</p>"
+
+// Generamos el color aleatorio del fondo
+for (i = 0; i < 2000; i++) {
+    var a = Math.floor(Math.random() * 881);
+    var b = Math.floor(Math.random() * 1000);
+    var color = "#";
+
+    for (j = 0; j < 6; j++) {
+        color += Math.floor(Math.random() * 9);
+    }
+}
+
+document.body.style.background = color;
