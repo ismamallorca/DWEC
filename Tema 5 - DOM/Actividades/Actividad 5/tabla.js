4cbc9afe6b3fb3c823e611b42e70c60127db497c
= <=>
2022-02-09 16:06:35 +0100

    update

---
+++ Tema 5 - DOM/Actividades/Actividad 5/tabla.js
@@ -0,0 +1,9 @@
+function borrarCamposTexto() {
+    document.getElementById('text').value = '';
+    document.getElementById('fila').value = '';
+    document.getElementById('columna').value = '';
+}
+
+function agregarColumna(tablaId, numColumnaId, textoId) {
+    let tbodyTabla = document.getElementById(tablaId).tBodies[0];
+}
